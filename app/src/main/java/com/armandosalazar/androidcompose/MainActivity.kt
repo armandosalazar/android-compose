a9715eacefcd489f4e74daa188dd2f8e2a775906
Armando Salazar <armandosalazar351@gmail.com>
2024-01-11 19:50:31 -0600

    feat: use box layout

--- app/src/main/java/com/armandosalazar/androidcompose/MainActivity.kt
+++ app/src/main/java/com/armandosalazar/androidcompose/MainActivity.kt
@@ -3,12 +3,16 @@ package com.armandosalazar.androidcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.armandosalazar.androidcompose.ui.theme.AndroidComposeTheme
 
@@ -37,10 +41,22 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    widthDp = 400,
+    heightDp = 200
+)
 @Composable
 fun GreetingPreview() {
     AndroidComposeTheme {
-        Greeting("Android")
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.LightGray),
+            contentAlignment = Alignment.Center
+        ) {
+            Greeting("Android")
+            Greeting(name = "Armando", modifier = Modifier.align(Alignment.BottomEnd))
+        }
     }
 }
